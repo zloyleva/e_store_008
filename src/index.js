e1d0c1cee29a4f9efc9e746b320956ccb4b19cb7
ZloyLeva <zloyleva@gmail.com>
2018-11-18 18:20:15 +0200

    Add login

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import {Data} from "./data";
 import {PageData} from "./page_data";
 import {Catalog} from "./catalog";
 import {Pagination} from "./pagination"
+import {Login} from "./login"
 
-const per_page = 6;
+if(localStorage.getItem("isLogin")){
+	const per_page = 6;
 
-const catalog = new Catalog(per_page);
-catalog.renderProducts(Data.setProducts(Data.loadProducts()), PageData.getCurrentPage());
-
-const pagination = new Pagination(per_page);
-pagination.createPagination(Data.getProducts(), PageData.getCurrentPage());
+	const catalog = new Catalog(per_page);
+	catalog.renderProducts(Data.setProducts(Data.loadProducts()), PageData.getCurrentPage());
+	
+	const pagination = new Pagination(per_page);
+	pagination.createPagination(Data.getProducts(), PageData.getCurrentPage());
+}else{
+	const login = new Login();
+	login.createHtmlElement();
+}
---
+++ src/login.js
@@ -0,0 +1,41 @@
+import {Builder} from "./builder";
+
+export class Login{
+	constructor(per_page){
+		this.element = document.getElementById("login_container");
+	}
+
+	createHtmlElement(){
+
+		const labelEmail = Builder.createNewElement("label", "Email address", null,[{"name":"for", "value":"exampleInputEmail1"}]);
+		const emailInput = Builder.createNewElement("input", null, "form-control",[
+			{"name":"id", "value":"exampleInputEmail1"},
+			{"name":"placeholder", "value":"Enter email"}
+		]);
+		const small = Builder.createNewElement("small", "We'll never share your email with anyone else.", "form-text text-muted",[{"name":"id", "value":"emailHelp"}]);
+
+		let divEmail = Builder.attachChilderToParent(Builder.createNewElement("div", null, "form-group"), [labelEmail,emailInput,small]);
+
+
+		const labelPass = Builder.createNewElement("label", "Password", null,[{"name":"for", "value":"exampleInputPassword1"}]);
+		const passInput = Builder.createNewElement("input", null, "form-control",[
+			{"name":"id", "value":"exampleInputPassword1"},
+			{"name":"placeholder", "value":"Password"},
+			{"name":"type", "value":"password"},
+		]);
+
+		let divPass = Builder.attachChilderToParent(Builder.createNewElement("div", null, "form-group"), [labelPass,passInput]);
+
+		const button = Builder.createNewElement("button", "Login", "btn btn-primary",[{"name":"type", "value":"submit"}]);
+
+		let form = Builder.attachChilderToParent(Builder.createNewElement("form", null, null, [{"name":"id", "value":"login_form"}]), [divEmail, divPass, button]);
+
+		this.element.appendChild(form);
+
+		document.getElementById("login_form").addEventListener("submit", this.submit, false);
+	}
+
+	submit(){
+		localStorage.setItem("isLogin", true);
+	}
+}
